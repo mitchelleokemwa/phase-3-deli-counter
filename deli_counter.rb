6c9abf031fa4effe0f08127d740c5a59392ad810
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-02-21 23:26:33 +0300

    Completed assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line (array)
+    puts array.length == 0 ? "The line is currently empty." :
+     "The line is currently: #{array.map.with_index{
+        |name,  index|"#{index + 1}. #{name}" }.join(" ")}"
+end
+
+def take_a_number(array, name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving (array)
+    puts array.length == 0 ? "There is nobody waiting to be served!" : "Currently serving #{array.first}."
+    array = array.shift()
+end
